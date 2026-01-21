884a3d88abe6bc8561102277f82b1a2346e2a4c7
Jens Maus <mail@jens-maus.de>
2026-01-21 15:33:39 +0100

    refactored qutselect_connect_app to use xosd for status display in downloading+installing packages and reworked yad dialog request for questioning the BBB room-id that a user might try to connect to.

--- scripts/qutselect_connect_app.sh
+++ scripts/qutselect_connect_app.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# shellcheck shell=dash disable=SC3010,SC3020
+# shellcheck shell=dash disable=SC3010,SC3020,SC3057,SC3004,SC3060,SC2030,SC2031
 #
 # This is a startup script for qutselect which initates a
-# VNC session to a windows server via 'vncviewer'
+# third-party application download+install in a thinRoot
+# environment where qutselect is used.
 #
 # It receives the following inputs:
 #
@@ -41,27 +42,55 @@ umask 077
 
 TMPDIR=/tmp
 
+# font descriptions
+YAD_FONT_DESC="${YAD_FONT_DESC:-DejaVu Sans 18}"
+OSD_FONT_DESC="${OSD_FONT_DESC:--*-fixed-bold-r-*-*-30-*-*-*-*-*-*-*}"
+
+# osd_cat usage helpers
+osd_pid=
+
+osd_splash() {
+  local msg="$1"
+  local color="${2:-orange}"
+  local time="${3:-0}"
+  if [ -x /usr/bin/osd_cat ]; then
+    export LANG=en_US.UTF-8
+    echo "${msg}" | /usr/bin/osd_cat -A center -p top -f "${OSD_FONT_DESC}" -c "${color}" -s 5 -d "${time}" &
+    osd_pid=$!
+  else
+    osd_pid=
+  fi
+}
+
+osd_close() {
+  if [[ -n "${osd_pid}" ]]; then
+    kill "${osd_pid}" 2>/dev/null
+    wait "${osd_pid}" 2>/dev/null
+    osd_pid=
+  fi
+}
+trap 'osd_close' EXIT INT TERM
+
 res=2
 if [[ "${app}" == "zoom" ]]; then
 
   if [[ ! -d /opt/zoom ]]; then
-    yad --center --text="Downloading zoom.pkg...." --title "Zoom Installation" --no-buttons &
-    yad_pid=$!
+    osd_splash "Downloading zoom.pkg..."
     /usr/bin/wget -q "${BASE_PATH}/pkgs/zoom.pkg" -O "${TMPDIR}/zoom.pkg"
-    kill -9 ${yad_pid}
+    osd_close
 
-    yad --center --text="Installing zoom.pkg...." --title "Zoom Installation" --no-buttons &
-    yad_pid=$!
+    osd_splash "Installing zoom.pkg..."
     tar -C / -xf "${TMPDIR}/zoom.pkg"
     rm -f "${TMPDIR}/zoom.pkg"
-    kill -9 ${yad_pid}
+    osd_close
   fi
 
   if [[ ! -x /opt/zoom/ZoomLauncher ]]; then
-    yad --center --text="ERROR: Installation of Zoom failed"
+    osd_splash "ERROR: Installation of Zoom failed" "red" 3
+    wait "${osd_pid}" 2>/dev/null
+    exit 1
   else
-    yad --center --text="Starting zoom..." --no-buttons &
-    yad_pid=$!
+    osd_splash "Starting zoom..."
 
     # remove all previous data
     rm -rf "${HOME}/.zoom" "${HOME}/.config/zoom.conf" "${HOME}/.config/zoomus.conf"
@@ -90,29 +119,28 @@ if [[ "${app}" == "zoom" ]]; then
     USER="Enter your name here" /opt/zoom/ZoomLauncher >"/tmp/zoom-${USER}-$$.log" 2>&1 &
     res=$?
 
-    kill -9 ${yad_pid}
+    osd_close
   fi
 
 elif [[ "${app}" == "chrome" ]] || [[ "${app}" == "bbb" ]]; then
 
   if [[ ! -d /opt/chrome ]]; then
-    yad --center --text="Downloading chrome.pkg...." --title "Chrome Installation" --no-buttons &
-    yad_pid=$!
+    osd_splash "Downloading chrome.pkg..."
     /usr/bin/wget -q "${BASE_PATH}/pkgs/chrome.pkg" -O "${TMPDIR}/chrome.pkg"
-    kill -9 ${yad_pid}
+    osd_close
 
-    yad --center --text="Installing chrome.pkg...." --title "Chrome Installation" --no-buttons &
-    yad_pid=$!
+    osd_splash "Installing chrome.pkg..."
     tar -C / -xf "${TMPDIR}/chrome.pkg"
     rm -f "${TMPDIR}/chrome.pkg"
-    kill -9 ${yad_pid}
+    osd_close
   fi
 
   if [[ ! -x /opt/chrome/chrome ]]; then
-    yad --center --text="ERROR: Installation of Chrome failed"
+    osd_splash "ERROR: Installation of Chrome failed" "red" 3
+    wait "${osd_pid}" 2>/dev/null
+    exit 1
   else
-    yad --center --text="Starting chrome..." --no-buttons &
-    yad_pid=$!
+    osd_splash "Starting chrome..."
 
     # remove all previous data
     rm -rf "${HOME}/.config/chrome"
@@ -125,17 +153,70 @@ elif [[ "${app}" == "chrome" ]] || [[ "${app}" == "bbb" ]]; then
 
     # add some options for BBB mode
     if [[ "${app}" == "bbb" ]]; then
-      CMDOPT="${CMDOPT} --app=https://bbb.hzdr.de/"
-      if [[ "${resolution}" == "fullscreen" ]]; then
-        CMDOPT="${CMDOPT} --kiosk"
-      fi
+      (
+        # when we start a BBB session we have to ask for a
+        # potential room identifier
+        BBB_BASE="https://bbb.hzdr.de/"
+        ROOM_RE='^[A-Za-z0-9]{3}(-[A-Za-z0-9]{3}){3}$'   # xxx-xxx-xxx-xxx
+        room=""
+
+        while true; do
+          room="$(
+            yad --entry \
+                --title="https://bbb.hzdr.de/b/xxx-xxx-xxx-xxx" \
+                --entry-text="${room}" \
+                --text="<span font_desc=\"${YAD_FONT_DESC}\"><b>Please enter BBB Room-ID:</b></span>\n\nFormat: xxx-xxx-xxx-xxx (part after /b/ in link)" \
+                --window-type=dialog --on-top --sticky --center --fixed \
+                --button="OK":0 --button="No Room ID":1 --button="Cancel":2
+          )"
+          rc=$?
+
+          if [[ $rc -eq 2 ]] || [[ $rc -eq 252 ]]; then
+            exit 0
+          elif [[ $rc -eq 1 ]] || [[ -z "${room}" ]]; then
+            room=""
+            break
+          fi
+
+          # Optional: remote whitespaces and if someone added the url, extract it
+          room="${room//[[:space:]]/}"
+          room="${room#"${BBB_BASE}"b/}" # removes "https://bbb.hzdr.de/b/" if there
+          room=$(echo "${room}" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+
+          if [[ "$room" =~ $ROOM_RE ]]; then
+            break
+          fi
+
+          yad --error \
+              --title="Invalid Room-ID" \
+              --text="<span font_desc=\"${YAD_FONT_DESC}\"><b>The entered Room-ID was invalid.</b></span>\n\nExpected: xxx-xxx-xxx-xxx (each 3 alphanumerical characters)." \
+              --window-type=dialog --on-top --center --fixed \
+              --button="Retry":0
+        done
+
+        if [[ -n "${room}" ]]; then
+          bbb_url="${BBB_BASE}b/${room}"
+        else
+          bbb_url="${BBB_BASE}"
+        fi
+
+        CMDOPT="${CMDOPT} --app=${bbb_url}"
+        if [[ "${resolution}" == "fullscreen" ]]; then
+          CMDOPT="${CMDOPT} --kiosk"
+        fi
+
+        # shellcheck disable=SC2086
+        /opt/chrome/chrome ${CMDOPT} --test-type --noerrdialogs --no-first-run --disable-translate --disk-cache-dir=/dev/null --disable-extensions >"/tmp/chrome-${USER}-$$.log" 2>&1 &
+        osd_close
+      ) </dev/null >/dev/null 2>&1 &
+
+      # parent should return immediately
+      exit 0
+    else
+      /opt/chrome/chrome ${CMDOPT} --test-type --noerrdialogs --no-first-run --disable-translate --disk-cache-dir=/dev/null --disable-extensions >"/tmp/chrome-${USER}-$$.log" 2>&1 &
+      res=$?
+      osd_close
     fi
-
-    # shellcheck disable=SC2086
-    /opt/chrome/chrome ${CMDOPT} --test-type --noerrdialogs --no-first-run --disable-translate --disk-cache-dir=/dev/null --no-sandbox --disable-extensions >"/tmp/chrome-${USER}-$$.log" 2>&1 &
-    res=$?
-
-    kill -9 ${yad_pid}
   fi
 
 elif [[ "${app}" == "firefox" ]]; then
--- src/CMainWindow.cpp
+++ src/CMainWindow.cpp
@@ -1,7 +1,7 @@
 /* vim:set ts=2 nowrap: ****************************************************
 
  qutselect - A simple Qt-based GUI frontend for remote terminals
- Copyright (C) 2005-2025 by Jens Maus <mail@jens-maus.de>
+ Copyright (C) 2005-2026 by Jens Maus <mail@jens-maus.de>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -519,7 +519,7 @@ CMainWindow::CMainWindow(CApplication* app)
     resize(m_pSettings->value("size", QSize(WINDOW_WIDTH, WINDOW_HEIGHT)).toSize());
   }
   
-  setWindowTitle("qutselect " + QString(PROJECT_VERSION) + " - (c) 2005-2025 hzdr.de");
+  setWindowTitle("qutselect " + QString(PROJECT_VERSION) + " - (c) 2005-2026 hzdr.de");
 
   LEAVE();
 }
